e74d7344631583a1e9d9958ba88232dba9b8f2b9
mikertb <mike@rtb.cat>
2016-05-01 19:39:55 +0800

    Disabled portal & added appnexus library for easy dev.

---
+++ application/controllers/Temp.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* 
+*/
+class Temp extends CI_Controller
+{
+    
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        echo '<h2 style="text-align:center">Site is still under construction.</h2>';
+    }
+}
---
+++ application/libraries/Appnexus/config.php
@@ -0,0 +1,39 @@
+<?php
+/*
++-------------------------------------------------------------
+| Appnexus API Configuration File
++-------------------------------------------------------------
+*/
+
+// Base endpoint (trailing slash included).
+$config['api_base'] = "http://api.appnexus.com/";
+
+// Can have multiple API users identified by array index.
+$config['api_users'][0] = array(
+    'name' => 'stats_api',
+    'pass' => 'Stats?3nt3r!'
+);
+$config['api_users'][1] = array(
+    'name' => 'cyrilnet',
+    'pass' => 'C3j3nt3r!'
+);
+
+// Token expiration time in seconds.
+$config['token_expiry'] = 7200; # 2 hrs default.
+
+// Token refresh interval in seconds.
+$config['token_refresh'] = 6600; # 1.83 hrs.
+
+// Date format to use.
+$config['date_format'] = "Y-m-d H-i-s T";
+
+// Number of login attempts to failure.
+$config['max_login_retry'] = 3;
+
+// Error logging option.
+$config['enable_error_logs'] = true;
+
+/* Do not add or edit anything below this line.
++------------------------------------------------------------
+*/
+$this->config = $config;

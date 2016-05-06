b92053824badf4876774c7a067a9644f974757fe
mikertb <mike@rtb.cat>
2016-05-07 02:51:02 +0800

    Frontpage opened.

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'temp';
+$route['default_controller'] = 'dashboard';
 $route['404_override'] = 'my404';
 $route['translate_uri_dashes'] = FALSE;

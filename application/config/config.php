58f58a2c0b28d234a37fc0c221689e5bf66211de
Jaiho <jai2.labrador@gmail.com>
2016-05-18 09:02:21 +0800

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'https://stats.rtb.cat/';
-$config['base_url'] = 'http://localhost/WPOnline/rtbcat/caravel/';
+$config['base_url'] = 'https://stats.rtb.cat/';
+// $config['base_url'] = 'http://localhost/WPOnline/rtbcat/caravel/';
 
 /*
 |--------------------------------------------------------------------------

daecb9f5cdbc3940cf64612d3c0e56abf81749b5
mikertb <mike@rtb.cat>
2016-05-19 01:39:27 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'https://stats.rtb.cat/';
-// $config['base_url'] = 'http://localhost/WPOnline/rtbcat/caravel/';
 
 /*
 |--------------------------------------------------------------------------

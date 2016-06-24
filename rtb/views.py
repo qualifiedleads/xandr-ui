2b3b98d3d00506bbce6d534e52e1b4c082f1bfd8
Alexey Khudobin <alexey.khudobin@gmail.com>
2016-06-24 07:56:46 +0300

    remove invalid transaction

--- rtb/views.py
+++ rtb/views.py
@@ -40,28 +40,6 @@ def stats(request):
       cvr=(Sum('post_click_convs') + Sum('post_view_convs'))/Sum('imps'),
       ctr=Sum('clicks') / Sum('imps'),
     )
-    # booked_revenue = models.DecimalField(max_digits=35, decimal_places=10)
-    # clicks = models.IntegerField(null=True, blank=True)
-    # click_thru_pct = models.FloatField(null=True, blank=True)
-    # convs_per_mm = models.FloatField(null=True, blank=True)
-    # convs_rate = models.FloatField(null=True, blank=True)
-    # cost_ecpa = models.DecimalField(max_digits=35, decimal_places=10)
-    # cost_ecpc = models.DecimalField(max_digits=35, decimal_places=10)
-    # cpm = models.DecimalField(max_digits=35, decimal_places=10)
-    # ctr = models.FloatField(null=True, blank=True)
-    # imps = models.IntegerField(null=True, blank=True)
-    # media_cost = models.DecimalField(max_digits=35, decimal_places=10)
-    # post_click_convs = models.IntegerField(null=True, blank=True)
-    # post_click_convs_rate = models.FloatField(null=True, blank=True)
-    # post_view_convs = models.IntegerField(null=True, blank=True)
-    # post_view_convs_rate = models.FloatField(null=True, blank=True)
-    # profit = models.DecimalField(max_digits=35, decimal_places=10)
-    # profit_ecpm = models.DecimalField(max_digits=35, decimal_places=10)
-    # imps_viewed = models.IntegerField(null=True, blank=True)
-    # view_measured_imps = models.IntegerField(null=True, blank=True)
-    # view_rate = models.FloatField(null=True, blank=True)
-    # view_measurement_rate = models.FloatField(null=True, blank=True)
-    #
     print q
     return JsonResponse({"message":list(q), "from_date":from_date,"to_date":to_date,})
 

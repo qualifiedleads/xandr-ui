0e185a1a389733b736609f520c1067789c31e970
Alexey Khudobin <alexey.khudobin@gmail.com>
2016-06-25 21:57:28 +0300

    Enable caching

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,19 @@ NEXUS_AUTH_DATA= {
     "username": "stats_api", 
     "password": "API?1nsid3!"
 }
+
+CACHES={
+    'default':{
+        'BACKEND':'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION':'rtb_cache_table',
+        'TIMEOUT':3600,
+        'OPTIONS':{
+            'MAX_ENTRIES':10240,
+            'CULL_FREQUENCY':4,
+        },
+    }
+}
+
 INVALIDATE_TIME = datetime.timedelta(hours=23,minutes=30)
 MAX_REPORT_WAIT = datetime.timedelta(hours=1)
 USE_TRANSACTIONS = False

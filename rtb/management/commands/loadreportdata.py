88cb10dc65522d1794779b269568db8e502c2df0
Alexey Khudobin <alexey.khudobin@gmail.com>
2016-06-25 12:58:13 +0300

    Small improvment on logging

--- rtb/management/commands/loadreportdata.py
+++ rtb/management/commands/loadreportdata.py
@@ -4,6 +4,16 @@ from django.core.management import BaseCommand
 from rtb.cron import dayly_task, get_current_time
 import datetime
 
+class tee(object):
+    def __init__(self, _fd1, _fd2):
+        self.fd1 =_fd1
+        self.fd2 = _fd2
+    def write(self, text):
+        self.fd1.write(text)
+        self.fd2.write(text)
+    def flush(self):
+        self.fd1.flush()
+        self.fd2.flush()
 
 class Command(BaseCommand):
     help = """
@@ -18,10 +28,11 @@ Call without params - load all data for last 48 days
         is_first = True
         p_line = '-'*79+'\n'
         with open('rtb/logs/loadreportdata,log', 'w') as f:
+            t = tee (self.stdout, f)
             for i in xrange(0,20):
-                f.write(p_line)
-                f.write('Save data for %s \n'%current_day.strftime('%Y-%m-%d'))
-                f.write(p_line)
-                dayly_task(current_day, is_first, f)
+                t.write(p_line)
+                t.write('Save data for %s \n'%current_day.strftime('%Y-%m-%d'))
+                t.write(p_line)
+                dayly_task(current_day, is_first, t)
                 is_first = False
                 current_day += one_day

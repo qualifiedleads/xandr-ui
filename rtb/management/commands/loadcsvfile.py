954108fe3682a9114ffaa87caa7dee50cb04b82a
Alexey Khudobin <alexey.khudobin@gmail.com>
2016-07-05 10:55:23 +0300

    Command "loadcsvfile" almost ready

--- rtb/management/commands/loadcsvfile.py
+++ rtb/management/commands/loadcsvfile.py
@@ -1,6 +1,14 @@
 from django.core.management import BaseCommand
-from rtb.cron import dayly_task, get_current_time
+from rtb.cron import analize_csv
+from rtb import models
+from django.db.models import Model
+import os
 
+all_classes = [models.__dict__[k]
+               for k in models.__dict__
+               if issubclass(models.__dict__[k], Model)]
+
+all_report_types = {getattr(c, 'api_report_name'): c for c in all_classes if hasattr(c, 'api_report_name')}
 
 class Command(BaseCommand):
     help = """
@@ -11,7 +19,20 @@ Need two params :
 """
 
     def add_arguments(self, parser):
-        parser.add_argument('load_day', nargs='?', type=convert_date)
+        parser.add_argument('file_name', type=str)
+        parser.add_argument('report_type', type=str)
 
     def handle(self, **options):
-        self.stdout.write('loadreportdata called')
+        self.stdout.write('loadcsvfile called')
+        self.stdout.write(all_report_types)
+        file_name = options.get('file_name')
+        report_type = options.get('report_type')
+        if report_type not in all_report_types:
+            self.stdout.write('Unknown report type. Exit')
+            return
+        if os.path.isfile(file_name):
+            metadata = {}
+            analize_csv(file_name, all_report_types[report_type], metadata)
+        else:
+            self.stdout.write('File not found. Exit')
+            return

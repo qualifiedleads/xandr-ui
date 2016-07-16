43be35434af9c473b3493d2b8f43b9374cb38d1d
PerhunAndrey <perhun@softproject.com.ua>
2016-07-16 15:10:58 +0300

    fixed one migration with index

--- rtb/migrations/0052_auto_20160714_0410.py
+++ rtb/migrations/0052_auto_20160714_0410.py
@@ -7,7 +7,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('rtb', '0051_auto_20160713_1556'),
+        ('rtb', '0050_auto_20160712_1854'),
     ]
 
     operations = [

2422ad938991a9f724aa1eef1cbe205f795e5854
Alexey Khudobin <alexey.khudobin@gmail.com>
2016-07-23 15:33:17 +0300

    Add admin url

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,12 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from django.conf.urls import url
+from django.contrib import admin
+
 
 urlpatterns = [
+                  url(r'^admin/', admin.site.urls),
                   url(r'^api/v1/', include('rtb.urls')),
                   url(r'^docs/', include('rest_framework_docs.urls')),
                   url(r'^$', RedirectView.as_view(url='/client/index.html'))

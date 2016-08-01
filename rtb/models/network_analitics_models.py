1f0a447175063be20dc9e6e1d799c912ede8040e
Alexey Khudobin <alexey.khudobin@gmail.com>
2016-08-01 11:42:41 +0300

    Create new, changed migration

--- rtb/models/network_analitics_models.py
+++ rtb/models/network_analitics_models.py
@@ -1,13 +1,14 @@
 import datetime
-from django.db import models
+from django.db import models, transaction
 from pytz import utc
 from ..report import nexus_get_objects_by_id
-from .models import Placement
+from .models import Placement, Campaign, Site
 
 class NetworkAnalyticsReport_ByPlacement(models.Model):
     hour = models.DateTimeField(null=True, blank=True, db_index=True)
     placement = models.ForeignKey("Placement", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
     campaign = models.ForeignKey("Campaign", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    seller_member = models.ForeignKey("PlatformMember", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
     imps = models.IntegerField(null=True, blank=True)
     clicks = models.IntegerField(null=True, blank=True)
     cost = models.DecimalField(null=True, blank=True, max_digits=35, decimal_places=10)
@@ -19,16 +20,43 @@ class NetworkAnalyticsReport_ByPlacement(models.Model):
     def post_load(self, day):
         from_date = datetime.datetime(day.year, day.month, day.day, tzinfo=utc)
         to_date = datetime.datetime(day.year, day.month, day.day, 23, tzinfo=utc)
-        placements_mising = self.objects.filter(
+        campaigns_mising = self.objects.filter(
             hour__gte=from_date,
             hour__lte=to_date,
-            placement__id=None
-        ).values_list('placement_id', flat=True)
-        print placements_mising
-        # load all missing placements
-        saved_ids = nexus_get_objects_by_id(None,Placement,placements_mising)
-        if saved_ids !=placements_mising:
-            print 'Some objects not saved'
+            campaign__name=None
+        ).values_list('campaign_id', flat=True)
+        campaigns_mising = set(campaigns_mising)
+        saved_ids = nexus_get_objects_by_id(None,Campaign,campaigns_mising)
+        if saved_ids != campaigns_mising:
+            print 'Some campaigns not saved'
+        with transaction.atomic():
+            placements_mising = self.objects.filter(
+                hour__gte=from_date,
+                hour__lte=to_date,
+                placement__name=None
+            ).values_list('placement_id', flat=True)
+            placements_mising = set(placements_mising)
+            # load all missing placements
+            saved_ids = nexus_get_objects_by_id(None,Placement,placements_mising)
+            if saved_ids != placements_mising:
+                print 'Some placements not saved'
+            sites_missing = Placement.objects.filter(
+                pk__in=placements_mising,
+                site__name=None
+            ).values_list('site_id', flat=True)
+            sites_missing = set(sites_missing)
+            saved_ids = nexus_get_objects_by_id(None,Site,sites_missing)
+            if saved_ids != sites_missing:
+                print 'Some sites not saved'
+
+            publishers_missing = Placement.objects.filter(
+                pk__in=placements_mising,
+                publisher__name=None
+            ).values_list('publisher_id', flat=True)
+            publishers_missing = set(publishers_missing)
+            saved_ids = nexus_get_objects_by_id(None, Site, publishers_missing)
+            if saved_ids != publishers_missing:
+                print 'Some publishers not saved'
 
     class Meta:
         app_label = 'rtb'

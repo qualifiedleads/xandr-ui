41dd75a884759e587979ed68600c359698cee547
Alexey Khudobin <alexey.khudobin@gmail.com>
2016-08-12 23:34:28 +0300

    Work with load_appnexus_permissions ...(in progress0

--- rtb/appnexus_permissions.py
+++ rtb/appnexus_permissions.py
@@ -1,3 +1,17 @@
+import requests
+from django.conf import settings
+from models import FrameworkUser,User
+from report import get_auth_token
+import json
+
+appnexus_url = settings.__dict__.get(
+    'APPNEXUS_URL', 'https://api.appnexus.com/')
 
 def load_appnexus_permissions(user_id):
-    return True
+    url = appnexus_url + User.api_endpoint
+    token = get_auth_token()
+    r = requests.get(
+        url, params={'id':user_id}, headers={
+            "Authorization": token})
+    response = json.loads(r.content)['response']
+

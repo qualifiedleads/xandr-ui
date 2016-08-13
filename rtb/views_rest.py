fa5888d074a0d3970806b8f852bcccd257041724
Alexey Khudobin <alexey.khudobin@gmail.com>
2016-08-13 13:52:05 +0300

    Add permission check when manipulating users

--- rtb/views_rest.py
+++ rtb/views_rest.py
@@ -2,6 +2,8 @@ from django.contrib.admin.templatetags.admin_list import result_headers
 from rest_framework import filters
 from rest_framework import serializers
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+from rest_framework.exceptions import PermissionDenied
 
 from .models import NetworkAnalyticsRaw, FrameworkUser, Advertiser, User
 from django.contrib.auth.hashers import PBKDF2PasswordHasher, make_password, is_password_usable
@@ -12,16 +14,27 @@ class AdvertiserSerializer(serializers.ModelSerializer):
         model = Advertiser
         fields = '__all__'
 
+#generics.ListAPIView
 class AdvertiserViewSet(viewsets.ModelViewSet):
-    queryset = Advertiser.objects.all()
+    #queryset = Advertiser.objects.all()
 
     serializer_class = AdvertiserSerializer
+
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        #return Advertiser.objects.filter(purchaser=user)
+        return Advertiser.objects.all()
 
 
 class NetworkAnalyticsRawSerializer(serializers.ModelSerializer):
     class Meta:
         model = NetworkAnalyticsRaw
-        fields = ()
+        fields = '__all__'#()
 
 
 class UsersSerializer(serializers.ModelSerializer):
@@ -47,6 +60,7 @@ class UsersViewSet(viewsets.ModelViewSet):
     queryset = FrameworkUser.objects.all()
     # PBKDF2PasswordHasher
     serializer_class = UsersSerializer
+    permission_classes = (IsAdminUser,)
 
     def transform_password(self, request):
         if 'password' in request.data:
@@ -55,14 +69,20 @@ class UsersViewSet(viewsets.ModelViewSet):
                 request.data['password'] = make_password(password)
 
     def create(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            raise PermissionDenied('For create new users, current user must be a superuser');
         self.transform_password(request)
         return super(UsersViewSet, self).create(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            raise PermissionDenied('For update users data, current user must be a superuser');
         self.transform_password(request)
         return super(UsersViewSet, self).update(request, *args, **kwargs)
 
     def partial_update(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            raise PermissionDenied('For update users data, current user must be a superuser');
         self.transform_password(request)
         return super(UsersViewSet, self).partial_update(request, *args, **kwargs)
 

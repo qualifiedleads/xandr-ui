0ba10323f45e3c74cf3a6f6f3dd4919cd3abbd02
Alexey Khudobin <alexey.khudobin@gmail.com>
2016-08-28 13:19:06 +0300

    Add migration(test tables)

--- rtb/models/test_load.py
+++ rtb/models/test_load.py
@@ -3,16 +3,16 @@ import common
 
 class NetworkAnalyticsReport_ByPlacement_test(models.Model): #, common.PostLoadMix):
     hour = models.DateTimeField(null=True, blank=True, db_index=True)
-    publisher = models.ForeignKey("Publisher", null=True, blank=True, db_constraint=False, on_delete=models.DO_NOTHING)
+    publisher = models.ForeignKey("Publisher", null=True, blank=True, db_constraint=False, related_name="+", on_delete=models.DO_NOTHING)
     publisher_name = models.TextField(null=True, blank=True)
-    placement = models.ForeignKey("Placement", null=True, blank=True, db_constraint=False, on_delete=models.DO_NOTHING)
+    placement = models.ForeignKey("Placement", null=True, blank=True, db_constraint=False, related_name="+", on_delete=models.DO_NOTHING)
     placement_name = models.TextField(null=True, blank=True)
-    campaign = models.ForeignKey("Campaign", null=True, blank=True, db_constraint=False, on_delete=models.DO_NOTHING)
+    campaign = models.ForeignKey("Campaign", null=True, blank=True, db_constraint=False, related_name="+", on_delete=models.DO_NOTHING)
     campaign_name = models.TextField(null=True, blank=True)
     seller_member = models.ForeignKey("PlatformMember", null=True, blank=True, db_constraint=False,
-                                      on_delete=models.DO_NOTHING)
+                                      related_name="+", on_delete=models.DO_NOTHING)
     seller_member_name = models.TextField(null=True, blank=True)
-    creative = models.ForeignKey("Creative", null=True, blank=True, db_constraint=False, on_delete=models.DO_NOTHING)
+    creative = models.ForeignKey("Creative", null=True, blank=True, db_constraint=False, related_name="+", on_delete=models.DO_NOTHING)
     creative_name = models.TextField(null=True, blank=True)
     size = models.TextField(null=True, blank=True)
     imps = models.IntegerField(null=True, blank=True)
@@ -23,9 +23,9 @@ class NetworkAnalyticsReport_ByPlacement_test(models.Model): #, common.PostLoadM
     view_measured_imps = models.IntegerField(null=True, blank=True)
     view_rate = models.FloatField(null=True, blank=True)
     view_measurement_rate = models.FloatField(null=True, blank=True)
-    site = models.ForeignKey("Site", null=True, blank=True, db_constraint=False, on_delete=models.DO_NOTHING)
+    site = models.ForeignKey("Site", null=True, blank=True, db_constraint=False, related_name="+", on_delete=models.DO_NOTHING)
     site_name = models.TextField(null=True, blank=True)
-    geo_country = models.ForeignKey("Country", null=True, blank=True, db_constraint=False, on_delete=models.DO_NOTHING)
+    geo_country = models.ForeignKey("Country", null=True, blank=True, db_constraint=False, related_name="+", on_delete=models.DO_NOTHING)
     bid_type = models.TextField(null=True, blank=True)
     imp_type_id = models.IntegerField(null=True, blank=True)
 
@@ -39,21 +39,21 @@ class NetworkAnalyticsReport_ByPlacement_test(models.Model): #, common.PostLoadM
 
 class NetworkCarrierReport_Simple_test(models.Model): #, common.PostLoadMix):
     day = models.DateTimeField(null=True, blank=True, db_index=True)
-    carrier = models.ForeignKey("Carrier", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    carrier = models.ForeignKey("Carrier", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     carrier_name = models.TextField(null=True, blank=True)
     device_type = models.TextField(null=True, blank=True)
     connection_type = models.TextField(null=True, blank=True)
-    seller_member = models.ForeignKey("PlatformMember", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    seller_member = models.ForeignKey("PlatformMember", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     seller_member_name = models.TextField(null=True, blank=True)
     seller_type = models.TextField(null=True, blank=True)
-    campaign = models.ForeignKey("Campaign", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    campaign = models.ForeignKey("Campaign", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     campaign_name = models.TextField(null=True, blank=True)
-    media_type = models.ForeignKey("MediaType", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    media_type = models.ForeignKey("MediaType", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     media_type_name = models.TextField(null=True, blank=True,db_column= 'media_type') # Need special loading
     size = models.TextField(null=True, blank=True)
-    geo_country = models.ForeignKey("Country", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    geo_country = models.ForeignKey("Country", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     geo_country_name = models.TextField(null=True, blank=True)
-    publisher = models.ForeignKey("Publisher", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    publisher = models.ForeignKey("Publisher", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     publisher_name = models.TextField(null=True, blank=True)
     imps = models.IntegerField(null=True, blank=True)
     clicks = models.IntegerField(null=True, blank=True)
@@ -70,26 +70,26 @@ class NetworkCarrierReport_Simple_test(models.Model): #, common.PostLoadMix):
 
 class NetworkDeviceReport_Simple_test(models.Model): #, common.PostLoadMix):
     day = models.DateTimeField(null=True, blank=True, db_index=True)
-    device_make = models.ForeignKey("DeviceMake", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    device_make = models.ForeignKey("DeviceMake", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     device_make_name = models.TextField(null=True, blank=True)
-    device_model = models.ForeignKey("DeviceModel", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    device_model = models.ForeignKey("DeviceModel", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     device_model_name = models.TextField(null=True, blank=True)
     device_type = models.TextField(null=True, blank=True)
     connection_type = models.TextField(null=True, blank=True)
-    operating_system = models.ForeignKey("OperatingSystemExtended", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    operating_system = models.ForeignKey("OperatingSystemExtended", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     operating_system_name = models.TextField(null=True, blank=True)
-    browser = models.ForeignKey("Browser", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    browser = models.ForeignKey("Browser", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     browser_name = models.TextField(null=True, blank=True)
     seller_member = models.ForeignKey("PlatformMember", related_name='+', null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
     seller_member_name = models.TextField(null=True, blank=True)
     seller_type = models.TextField(null=True, blank=True)
-    campaign = models.ForeignKey("Campaign", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    campaign = models.ForeignKey("Campaign", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     campaign_name = models.TextField(null=True, blank=True)
-    media_type = models.ForeignKey("MediaType", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    media_type = models.ForeignKey("MediaType", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     media_type_name = models.TextField(null=True, blank=True,db_column='media_type') # Need special loading
-    geo_country = models.ForeignKey("Country", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    geo_country = models.ForeignKey("Country", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     geo_country_name = models.TextField(null=True, blank=True)
-    publisher = models.ForeignKey("Publisher", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    publisher = models.ForeignKey("Publisher", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     publisher_name = models.TextField(null=True, blank=True)
     imps = models.IntegerField(null=True, blank=True)
     clicks = models.IntegerField(null=True, blank=True)
@@ -106,18 +106,18 @@ class NetworkDeviceReport_Simple_test(models.Model): #, common.PostLoadMix):
 
 class GeoAnaliticsReport_test(models.Model):
     day = models.DateField(null=True, blank=True, db_index=True)  # Yes	The year, month, and day in which the auction took place.
-    member = models.ForeignKey("Member", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING) # Yes	The ID of the member.
+    member = models.ForeignKey("Member", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING) # Yes	The ID of the member.
     advertiser_currency = models.TextField(null=True, blank=True)  # Yes	The type of currency used by the advertiser.
-    insertion_order = models.ForeignKey("InsertionOrder", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)  # Yes	The insertion order ID.
-    campaign = models.ForeignKey("Campaign", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)  # Yes	The campaign ID.
-    advertiser = models.ForeignKey("Advertiser", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)  # Yes	The advertiser ID. If the value is 0, either the impression was purchased by an external buyer, or a default or PSA was shown. For more information on defaults and PSAs, see Network Reporting.
-    line_item = models.ForeignKey("LineItem", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)  # Yes	The line item ID.
-    geo_country = models.ForeignKey("Country", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)  # Yes	The country ID of the user's location as defined by the Country Service. 250 is shown in cases where we don't know the country or if the country doesn't map correctly to a location in our database.
-    geo_region = models.ForeignKey("Region", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)  # Yes	The region ID of the user's location as defined by the Region Service. 4291 is shown in cases where we don't know the region or if the region doesn't map correctly to a location in our database.
+    insertion_order = models.ForeignKey("InsertionOrder", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)  # Yes	The insertion order ID.
+    campaign = models.ForeignKey("Campaign", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)  # Yes	The campaign ID.
+    advertiser = models.ForeignKey("Advertiser", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)  # Yes	The advertiser ID. If the value is 0, either the impression was purchased by an external buyer, or a default or PSA was shown. For more information on defaults and PSAs, see Network Reporting.
+    line_item = models.ForeignKey("LineItem", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)  # Yes	The line item ID.
+    geo_country = models.ForeignKey("Country", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)  # Yes	The country ID of the user's location as defined by the Country Service. 250 is shown in cases where we don't know the country or if the country doesn't map correctly to a location in our database.
+    geo_region = models.ForeignKey("Region", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)  # Yes	The region ID of the user's location as defined by the Region Service. 4291 is shown in cases where we don't know the region or if the region doesn't map correctly to a location in our database.
     geo_country_name = models.TextField(null=True, blank=True)  # No	The name of the user's country, as defined by the Country Service.
     geo_region_name = models.TextField(null=True, blank=True)  # No	The name of the region of the user's location as defined by the Region Service.
-    geo_dma = models.ForeignKey("DemographicArea", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING) # No	The name and ID of the demographic area where the user is located, in the format "New York NY (501)". The string "unknown values (-1)" can appear in cases where we don't know the demographic area or if the demographic area doesn't map correctly to a location in our database.
-    pixel = models.ForeignKey("ConversionPixel", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)  # Yes The unique identification number of the conversion pixel.
+    geo_dma = models.ForeignKey("DemographicArea", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING) # No	The name and ID of the demographic area where the user is located, in the format "New York NY (501)". The string "unknown values (-1)" can appear in cases where we don't know the demographic area or if the demographic area doesn't map correctly to a location in our database.
+    pixel = models.ForeignKey("ConversionPixel", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)  # Yes The unique identification number of the conversion pixel.
     imps = models.IntegerField(null=True, blank=True)  # imps	The total number of impressions (served and resold).
     clicks = models.IntegerField(null=True, blank=True)  # clicks	The total number of clicks across all impressions.
     cost = models.DecimalField(max_digits=16, decimal_places=2, null=True, blank=True)  # cost	The total cost of the inventory purchased.
@@ -144,14 +144,14 @@ class GeoAnaliticsReport_test(models.Model):
 class SiteDomainPerformanceReport_test(models.Model):
     day = models.DateTimeField(null=True, blank=True, db_index=True)
     site_domain = models.TextField(null=True, blank=True, db_index=True)
-    campaign = models.ForeignKey("Campaign", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
-    line_item = models.ForeignKey("LineItem", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
-    top_level_category = models.ForeignKey("ContentCategory", related_name='top_level_category_id', null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
-    second_level_category = models.ForeignKey("ContentCategory", related_name='second_level_category_id', null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
-    deal = models.ForeignKey("Deal", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
-    advertiser = models.ForeignKey("Advertiser", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
-    buyer_member = models.ForeignKey("Member", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
-    operating_system = models.ForeignKey("OperatingSystemExtended", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    campaign = models.ForeignKey("Campaign", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
+    line_item = models.ForeignKey("LineItem", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
+    top_level_category = models.ForeignKey("ContentCategory", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
+    second_level_category = models.ForeignKey("ContentCategory", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
+    deal = models.ForeignKey("Deal", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
+    advertiser = models.ForeignKey("Advertiser", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
+    buyer_member = models.ForeignKey("Member", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
+    operating_system = models.ForeignKey("OperatingSystemExtended", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     supply_type = models.TextField(null=True, blank=True)
     mobile_application_id =  models.TextField(null=True, blank=True, db_index=True)
     mobile_application_name = models.TextField(null=True, blank=True, db_index=True)
@@ -160,7 +160,7 @@ class SiteDomainPerformanceReport_test(models.Model):
     age_bucket = models.TextField(null=True, blank=True)
     gender = models.TextField(null=True, blank=True)
     is_remarketing = models.NullBooleanField()
-    conversion_pixel = models.ForeignKey("ConversionPixel", null=True, blank=True, db_constraint=False, on_delete = models.DO_NOTHING)
+    conversion_pixel = models.ForeignKey("ConversionPixel", null=True, blank=True, db_constraint=False, related_name="+", on_delete = models.DO_NOTHING)
     booked_revenue = models.DecimalField(null=True, blank=True, max_digits=35, decimal_places=10)
     clicks = models.IntegerField(null=True, blank=True)
     click_thru_pct = models.FloatField(null=True, blank=True)

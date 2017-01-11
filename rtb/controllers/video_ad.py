d0e6bb6f28adbd9bb44baaa57872f58703a7743b
Anton Khrolenko <hobdag@gmail.com>
2017-01-11 10:16:53 +0200

    Changed query for API and deleted totals from grid API on video adv page

--- rtb/controllers/video_ad.py
+++ rtb/controllers/video_ad.py
@@ -53,7 +53,7 @@ def apiSendVideoCampaingData(request):
                   campaign_id,
                   SUM(media_cost) AS sum_cost,
                   SUM(imps) AS sum_imps,
-                  case SUM(imps) when 0 then 0 else SUM(media_cost)::float/SUM(imps) end cpm
+                  case SUM(imps) when 0 then 0 else SUM(media_cost)/SUM(imps) end cpm
                 FROM
                   site_domain_performance_report
                 WHERE
@@ -69,13 +69,13 @@ def apiSendVideoCampaingData(request):
               LEFT JOIN(
                 SELECT
                   "CpId",
-                  SUM(cpvm::float) AS allcpvm,
+                  SUM(cpvm) AS allcpvm,
                   case COUNT("CpId") when 0 then 0 else COUNT("CpId") end ad_starts,
-                  case COUNT(id) when 0 then 0 else SUM(cpvm::float)/COUNT("CpId") end cpvm
+                  case COUNT(id) when 0 then 0 else SUM(cpvm)/COUNT("CpId") end cpvm
                 FROM
                   rtb_adstart_tracker
                 WHERE
-                  "AdvId" = '""" + str(advertiser_id) + """'
+                  "AdvId" = """ + str(advertiser_id) + """
                   AND
                   "Date" >='""" + str(from_date) + """'
                   AND
@@ -83,7 +83,7 @@ def apiSendVideoCampaingData(request):
                 GROUP BY
                   "CpId"
               ) AS video
-              ON report.campaign_id=video."CpId"::integer
+              ON report.campaign_id=video."CpId"
               LEFT JOIN(
                 SELECT
                   id,
@@ -109,24 +109,9 @@ def apiSendVideoCampaingData(request):
                 ON max_vac_date.max_date=v.date
               ) AS vac
               ON report.campaign_id=vac.campaign_id
-            """)
-
+            LIMIT """ + str(params["take"]) + """ OFFSET """ + str(params["skip"]))
     answer = {}
     answer["campaigns"] = []
-    answer["total_imp_hour"] = 0
-    answer["total_ad_starts_hour"] = 0
-    answer["total_spent_hour"] = 0
-    answer["total_cpm_hour"] = 0
-    answer["total_profit_loss_hour"] = 0
-    answer["total_profit_loss"] = 0
-    answer["total_cpvm_hour"] = 0
-    answer["total_spent_cpvm_hour"] = 0
-    answer["total_spent"] = 0
-    answer["total_sum_imps"] = 0
-    answer["total_cpm"] = 0
-    answer["total_allcpvm"] = 0
-    answer["total_ad_starts"] = 0
-    answer["total_cpvm"] = 0
 
     for row in queryRes:
         answer["campaigns"].append({
@@ -136,7 +121,7 @@ def apiSendVideoCampaingData(request):
             "ad_starts_hour": row.ad_starts_hour,
             "spent_hour": row.spent_hour,
             "cpm_hour": row.cpm_hour,
-            "fill_rate_hour": row.fill_rate_hour,
+            "fill_rate_hour": 0 if row.fill_rate_hour is None else row.fill_rate_hour*100,
             "profit_loss_hour": row.profit_loss_hour,
             "cpvm_hour": row.cpvm_hour,
             "spent_cpvm_hour": row.spent_cpvm_hour,
@@ -155,32 +140,9 @@ def apiSendVideoCampaingData(request):
             answer["campaigns"][-1]["fill_rate"] = 0
         else:
             answer["campaigns"][-1]["fill_rate"] = float(answer["campaigns"][-1]["ad_starts"]) / \
-                                                   answer["campaigns"][-1]["sum_imps"]
-        answer["campaigns"][-1]["profit_loss"] = answer["campaigns"][-1]["cpm"] * answer["campaigns"][-1][
-            "sum_imps"] - answer["campaigns"][-1]["cpvm"] * answer["campaigns"][-1]["ad_starts"]
-
-        answer["total_imp_hour"] += int(answer["campaigns"][-1]["imp_hour"])
-        answer["total_ad_starts_hour"] += int(answer["campaigns"][-1]["ad_starts_hour"])
-        answer["total_spent_hour"] += float(answer["campaigns"][-1]["spent_hour"])
-        answer["total_cpm_hour"] += float(answer["campaigns"][-1]["cpm_hour"])
-        answer["total_profit_loss_hour"] += float(answer["campaigns"][-1]["profit_loss_hour"])
-        answer["total_cpvm_hour"] += float(answer["campaigns"][-1]["cpvm_hour"])
-        answer["total_spent_cpvm_hour"] += float(answer["campaigns"][-1]["spent_cpvm_hour"])
-        answer["total_spent"] += float(answer["campaigns"][-1]["spent"])
-        answer["total_sum_imps"] += int(answer["campaigns"][-1]["sum_imps"])
-        answer["total_cpm"] += float(answer["campaigns"][-1]["cpm"])
-        answer["total_allcpvm"] += float(answer["campaigns"][-1]["allcpvm"])
-        answer["total_ad_starts"] += int(answer["campaigns"][-1]["ad_starts"])
-        answer["total_cpvm"] += float(answer["campaigns"][-1]["cpvm"])
-        answer["total_profit_loss"] += answer["campaigns"][-1]["profit_loss"]
-    if answer["total_sum_imps"] == 0:
-        answer["total_fill_rate"] = 0
-    else:
-        answer["total_fill_rate"] = float(answer["total_ad_starts"]) / answer["total_sum_imps"]
-    if answer["total_imp_hour"] == 0:
-        answer["total_fill_rate_hour"] = 0
-    else:
-        answer["total_fill_rate_hour"] = float(answer["total_ad_starts_hour"]) / answer["total_imp_hour"]
+                                                   answer["campaigns"][-1]["sum_imps"] * 100
+        answer["campaigns"][-1]["profit_loss"] = float(answer["campaigns"][-1]["cpm"]) * answer["campaigns"][-1][
+            "sum_imps"] - float(answer["campaigns"][-1]["cpvm"]) * answer["campaigns"][-1]["ad_starts"]
     answer["total_count"] = len(answer["campaigns"])
     return Response(answer)
 
@@ -228,7 +190,7 @@ def apiSendVideoCampaingStatistics(request):
               report.sum_imp AS imp,
               report.sum_spend AS spend,
               video.ad_s AS ad_starts,
-              case report.sum_imp when 0 then 0 else video.ad_s::float / report.sum_imp::float end fill_rate,
+              case report.sum_imp when 0 then 0 else video.ad_s::float / report.sum_imp end fill_rate,
               report.cpm * report.sum_imp - video.cpvm * video.ad_s AS profit_loss
             FROM
               site_domain_performance_report m_rep
@@ -236,11 +198,11 @@ def apiSendVideoCampaingStatistics(request):
                 SELECT
                   "Date"::timestamp::date AS dates,
                   COUNT("AdvId") AS ad_s,
-                  case COUNT("AdvId") when 0 then 0 else SUM(cpvm::float)/COUNT("AdvId") end cpvm
+                  case COUNT("AdvId") when 0 then 0 else SUM(cpvm)/COUNT("AdvId") end cpvm
                 FROM
                   rtb_adstart_tracker
                 WHERE
-                  "AdvId"='""" + str(advertiser_id) + """'
+                  "AdvId"=""" + str(advertiser_id) + """
                 AND
                   "Date" >= '""" + str(from_date) + """'
                 AND

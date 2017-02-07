69101ad92f32715dfe2ae7e3b5cb7668ce4235f5
BabenkoOleg <babenoko@gmail.com>
2017-02-07 18:02:21 +0200

    Added caption for current method

--- client/src/app/video/automaticcpm/acpmc.controller.js
+++ client/src/app/video/automaticcpm/acpmc.controller.js
@@ -16,6 +16,7 @@
     $rootScope.name = Campaign.campaign;
     $rootScope.line_item = Campaign.line_item;
     $rootScope.line_item_id = Campaign.line_item_id;
+    vm.currentAlgo = '';
 
     vm.campName = Campaign.campaign;
     vm.campId = Campaign.id;
@@ -57,88 +58,18 @@
       $state.go('home.main');
     }
 
-    AutomaticCpm.getList(Campaign.id).then(function (res) {
-      if (res == undefined) {
-        return;
-      }
+    AutomaticCpm.getList(Campaign.id)
+      .then(function (res) {
+        if (res == undefined) {
+          return;
+        }
 
-      vm.selectMethod = res.choice_list ? res.choice_list : null;
-      vm.cpmGraph = res.cpm_graph;
-      vm.fillrateGraph = res.fillrate_graph;
-      vm.profitGraph = res.profit_graph;
-    });
-
-    //region DATE PIKER
-    /** DATE PIKER **/
-    if ($localStorage.SelectedTime == null) {
-      $localStorage.SelectedTime = 0;
-      $localStorage.dataStart = $window.moment({ hour: '00' }).subtract(1, 'day').unix();
-      $localStorage.dataEnd = $window.moment({ hour: '00' }).subtract(1, 'day').endOf('day').unix();
-      vm.dataStart = $window.moment({ hour: '00' }).subtract(1, 'day').unix();
-      vm.dataEnd = $window.moment({ hour: '00' }).subtract(1, 'day').endOf('day').unix();
-    } else {
-      if ($localStorage.dataStart == null || $localStorage.dataEnd == null) {
-        $localStorage.SelectedTime = 0;
-        $localStorage.dataStart = $window.moment({ hour: '00' }).subtract(1, 'day').unix();
-        $localStorage.dataEnd = $window.moment({ hour: '00' }).subtract(1, 'day').endOf('day').unix();
-        vm.dataStart = $window.moment({ hour: '00' }).subtract(1, 'day').unix();
-        vm.dataEnd = $window.moment({ hour: '00' }).subtract(1, 'day').endOf('day').unix();
-      } else {
-        vm.dataStart = $localStorage.dataStart;
-        vm.dataEnd = $localStorage.dataEnd;
-      }
-    }
-
-    var products = [
-      {
-        ID: 0,
-        Name: LC('MAIN.DATE_PICKER.YESTERDAY'),
-        dataStart: $window.moment({ hour: '00' }).subtract(1, 'day').unix(),
-        dataEnd: $window.moment({ hour: '00' }).subtract(1, 'day').endOf('day').unix()
-      }, {
-        ID: 1,
-        Name: LC('MAIN.DATE_PICKER.LAST_3_DAYS'),
-        dataStart: $window.moment({ hour: '00' }).subtract(3, 'day').unix(),
-        dataEnd: $window.moment({ hour: '00' }).unix()
-      }, {
-        ID: 2,
-        Name: LC('MAIN.DATE_PICKER.LAST_7_DAYS'),
-        dataStart: $window.moment({ hour: '00' }).subtract(7, 'day').unix(),
-        dataEnd: $window.moment({ hour: '00' }).unix()
-      }, {
-        ID: 3,
-        Name: LC('MAIN.DATE_PICKER.LAST_14_DAYS'),
-        dataStart: $window.moment({ hour: '00' }).subtract(14, 'day').unix(),
-        dataEnd: $window.moment({ hour: '00' }).unix()
-      }, {
-        ID: 4,
-        Name: LC('MAIN.DATE_PICKER.LAST_21_DAYS'),
-        dataStart: $window.moment({ hour: '00' }).subtract(21, 'day').unix(),
-        dataEnd: $window.moment({ hour: '00' }).unix()
-      }, {
-        ID: 5,
-        Name: LC('MAIN.DATE_PICKER.CURRENT_MONTH'),
-        dataStart: $window.moment().startOf('month').unix(),
-        dataEnd: $window.moment().unix()
-      }, {
-        ID: 6,
-        Name: LC('MAIN.DATE_PICKER.LAST_MONTH'),
-        dataStart: $window.moment().subtract(1, 'month').startOf('month').unix(),
-        dataEnd: $window.moment().subtract(1, 'month').endOf('month').unix()
-      }, {
-        ID: 7,
-        Name: LC('MAIN.DATE_PICKER.LAST_90_DAYS'),
-        dataStart: $window.moment({ hour: '00' }).subtract(90, 'day').unix(),
-        dataEnd: $window.moment().unix()
-      }, {
-        ID: 8,
-        Name: LC('MAIN.DATE_PICKER.ALL_TIME'),
-        dataStart: 0,
-        dataEnd: $window.moment().unix()
-      }
-    ];
-
-    // endregion
+        vm.currentAlgo = res.current_algo;
+        vm.selectMethod = res.choice_list;
+        vm.cpmGraph = res.cpm_graph;
+        vm.fillrateGraph = res.fillrate_graph;
+        vm.profitGraph = res.profit_graph;
+      });
 
     vm.UI = {
       chartCPM: {
@@ -388,14 +319,14 @@
           vm.loadindicatorVisible = true;
           return AutomaticCpm.saveMethod($localStorage.advertiser.id, vm.campId, selectMethod)
             .then(function (res) {
-            if (res == false) {
-              vm.loadindicatorVisible = false;
-              return;
-            }
+              if (res == false) {
+                vm.loadindicatorVisible = false;
+                return;
+              }
 
-            vm.message = 'Save method: ' + res;
-            vm.loadindicatorVisible = false;
-          });
+              vm.message = 'Save method: ' + res;
+              vm.loadindicatorVisible = false;
+            });
         }
       },
       choiceList: {
@@ -407,18 +338,6 @@
         },
         onValueChanged: function (res) {
           selectMethod = res.value;
-        }
-      },
-      datePiker: {
-        items: products,
-        displayExpr: 'Name',
-        valueExpr: 'back_name',
-        value: products[$localStorage.SelectedTime].ID,
-        onValueChanged: function (e) {
-          $localStorage.SelectedTime = e.value;
-          $localStorage.dataStart = products[e.value].dataStart;
-          $localStorage.dataEnd = products[e.value].dataEnd;
-          $state.reload();
         }
       }
     };

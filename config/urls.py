13725385d84825677e70e1d49c82137febfd0482
BabenkoOleg <babenoko@gmail.com>
2017-11-06 17:47:50 +0200

    - Changed path for static files.

--- config/urls.py
+++ config/urls.py
@@ -4,12 +4,14 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.contrib.staticfiles.views import serve
 
 urlpatterns = [
                   url(r'^admin/', admin.site.urls),
                   url(r'^api/v1/', include('rtb.urls')),
                   url(r'^docs/', include('rest_framework_docs.urls')),
-                  url(r'^$', RedirectView.as_view(url='/client/dist/index.html'))
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  url(r'^$', serve, kwargs={'path': 'clientAngular/index.html'}),  #
+                  url(r'^(?!/?static/clientAngular/)(?!/?media/)(?P<path>.*\..*)$',
+                      RedirectView.as_view(url='/static/clientAngular/%(path)s', permanent=False)),
+              ]
 
--- rtb/management/commands/debug.py
+++ rtb/management/commands/debug.py
@@ -1,14 +1,14 @@
 from django.core.management import BaseCommand
 from rtb.campaign_rules import checkRules
 from rtb.models.placement_state import CampaignRules
+from rtb.cron import dayly_task
 
 class Command(BaseCommand):
 
     def handle(self, **options):
-        rule = CampaignRules.objects.get(pk=2)
-        state = checkRules(rule.campaign_id, rule.rules)
-        print state
+        dayly_task()
 
         # state = PlacementState(14574547, 7043440)
         # result = state.remove_placement_from_targets_list()
-        # print result
+        print "ssss"
+
